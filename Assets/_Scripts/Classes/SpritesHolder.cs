667c37a2f0c4b1513a8201204f83d5ec653ee416
Michael Woo <302maik@gmail.com>
2023-07-08 20:29:20 +0200

    Added sprite changes configuration to ACTION state

---
+++ Assets/_Scripts/Classes/SpritesHolder.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SpritesHolder {
+    // Hero sprites
+    public Sprite HeroIdle;
+    public Sprite HeroDamage;
+    public Sprite HeroHeal;
+    public Sprite HeroAttack;
+
+    // Monster sprites
+    public Sprite MonsterIdle;
+    public Sprite MonsterDamage;
+    public Sprite MonsterAttack;
+
+    
+}
--- Assets/_Scripts/Managers/Game/ActionStateManager.cs
+++ Assets/_Scripts/Managers/Game/ActionStateManager.cs
@@ -1,10 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class ActionStateManager : MonoBehaviour, IGameStateManager {
 
+    [Header("Actions configurations")]
+    [SerializeField]
+    [Range(0.1f, 1.0f)]
+    private float _timeBeforeReturningToIdle = 1.0f;
+
+    [Header("Inner game elements")]
+    [SerializeField]
+    private SpritesHolder _spritesHolder;
+    [SerializeField]
+    private GameObject _hero;
+    [SerializeField]
+    private List<GameObject> _monsters;
     private List<ActionStateEvents> _actionEvents;
+
+
     private float _timeBetweenEachAction;
 
     private void OnEnable() {
@@ -20,6 +35,7 @@ public class ActionStateManager : MonoBehaviour, IGameStateManager {
     }
     
     public void FinishState() {
+        ReturnToIdle();
         Messenger.Broadcast(GameEvents.FinishGameStateEvent);
     }
 
@@ -35,15 +51,80 @@ public class ActionStateManager : MonoBehaviour, IGameStateManager {
 
     private IEnumerator MakeActions(List<ActionStateEvents> actionEvents) {
         void MakeAction(ActionStateEvents actionEvent) {
-            // TODO set up sprites and animations for each actionEvent
-            Debug.Log($"Action! -> {actionEvent}");
+            Debug.Log($"{actionEvent}");
+            switch (actionEvent) {
+                case ActionStateEvents.DO_DAMAGE_TO_ENEMIES:
+                    DoDamageToEnemies();
+                    break;
+                case ActionStateEvents.TAKE_DAMAGE:
+                    TakeDamage();
+                    break;
+                case ActionStateEvents.HEAL:
+                    Heal();
+                    break;
+                case ActionStateEvents.KILL_ENEMY:
+                    KillEnemy();
+                    break;
+            }
         }
 
         foreach (var actionEvent in actionEvents) {
-            yield return new WaitForSeconds(_timeBetweenEachAction);
+            yield return new WaitForSeconds(_timeBeforeReturningToIdle);
             MakeAction(actionEvent);
+            yield return new WaitForSeconds(_timeBetweenEachAction);
+            ReturnToIdle();
         }
 
         FinishState();
+    }
+
+    void ChangeHeroSprite(Sprite sprite) {
+        var heroSpriteRenderer = _hero.GetComponent<SpriteRenderer>();
+        if (heroSpriteRenderer != null) {
+            heroSpriteRenderer.sprite = sprite;
+        }
+    }
+
+    void ChangeMonstersSprites(Sprite sprite) {
+        foreach (var monster in _monsters) {
+            if (monster.activeSelf) {
+                var monsterSpriteRenderer = monster.GetComponent<SpriteRenderer>();
+                if (monsterSpriteRenderer != null) {
+                    monsterSpriteRenderer.sprite = sprite;
+                }
+            }
+        }
+    }
+
+    private void DoDamageToEnemies() {
+        ChangeHeroSprite(_spritesHolder.HeroAttack);
+        ChangeMonstersSprites(_spritesHolder.MonsterDamage);
+    }
+
+    private void TakeDamage() {
+        ChangeHeroSprite(_spritesHolder.HeroDamage);
+        ChangeMonstersSprites(_spritesHolder.MonsterAttack);
+    }
+
+    private void Heal() {
+        ChangeHeroSprite(_spritesHolder.HeroHeal);
+        ChangeMonstersSprites(_spritesHolder.MonsterIdle);
+    }
+
+    private void KillEnemy() {
+        ChangeHeroSprite(_spritesHolder.HeroAttack);
+        ChangeMonstersSprites(_spritesHolder.MonsterDamage);
+
+        // TODO: This has to be done by the player in the UPDATE state
+        // var activeMonsters = _monsters.Select(monster => monster.activeSelf).ToList();
+        // if (activeMonsters.Count > 0) {
+        //     var monsterToKillIndex = Random.Range(0, activeMonsters.Count);
+        //     _monsters[monsterToKillIndex].SetActive(false);
+        // }
+    }
+
+    private void ReturnToIdle() {
+        ChangeHeroSprite(_spritesHolder.HeroIdle);
+        ChangeMonstersSprites(_spritesHolder.MonsterIdle);
     }
 }
