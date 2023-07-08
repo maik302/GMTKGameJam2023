70732362220ded2c947e52cc51f146fdcfda6150
Michael Woo <302maik@gmail.com>
2023-07-08 10:54:34 +0200

    Created base logic for UPDATE game state

--- Assets/_Scripts/Data/Static/GameEvents.cs
+++ Assets/_Scripts/Data/Static/GameEvents.cs
@@ -1,4 +1,5 @@
 public static class GameEvents {
     public const string InitStartStateEvent = "InitStartStateEvent"; 
     public const string InitActionStateEvent = "InitActionStateEvent"; 
+    public const string InitUpdateStateEvent = "InitUpdateStateEvent"; 
 }
---
+++ Assets/_Scripts/Managers/Game/UpdateStateManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateStateManager : MonoBehaviour, IGameStateManager {
+    private void OnEnable() {
+        Messenger.AddListener(GameEvents.InitUpdateStateEvent, StartState);
+    }
+
+    private void OnDisable() {
+        Messenger.RemoveListener(GameEvents.InitUpdateStateEvent, StartState);
+    }
+
+    public void FinishState(GameStates nextGameState) {
+        //TODO
+        Debug.Log($"The UPDATE state has finished!");
+    }
+
+    public void StartState() {
+        //TODO
+        Debug.Log($"The UPDATE state has finished!");
+    }
+}
